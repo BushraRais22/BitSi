759d4d8f980edf2a60e02caabc36e833dd4e603c
bushra <bushrais7903@gmail.com>
2024-12-02 22:11:40 +0500

    before chef

--- src/main/java/org/example/scd_db_project/controller/chef_ctrl.java
+++ src/main/java/org/example/scd_db_project/controller/chef_ctrl.java
@@ -1,12 +1,10 @@
 package org.example.scd_db_project.controller;
 
 import jakarta.servlet.http.HttpSession;
-import org.example.scd_db_project.model.Chef;
-import org.example.scd_db_project.model.ChefOrder;
-import org.example.scd_db_project.model.Customer;
-import org.example.scd_db_project.model.Restaurant;
+import org.example.scd_db_project.model.*;
 import org.example.scd_db_project.repository.chef_rep;
 import org.example.scd_db_project.repository.cheforder_rep;
+import org.example.scd_db_project.repository.chefrating_rep;
 import org.example.scd_db_project.repository.customer_rep;
 import org.example.scd_db_project.service.chef_service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +27,8 @@ public class chef_ctrl {
     private chef_rep ch_rep;
     @Autowired
     private cheforder_rep cheforder_rep;
+    @Autowired
+    private chefrating_rep chefrating_rep;
     @PostMapping("/signup")
     public String signup(
             @RequestParam ("ch_firstname") String first_name,
@@ -100,6 +100,53 @@ public class chef_ctrl {
         redirectAttributes.addFlashAttribute("message","Order placement Failed");
         return "redirect:/chef_ctrl/chefs";
     }
+    }
+    @GetMapping("/review")
+    public String getReview(@RequestParam("customerId") Integer customerId,
+                            @RequestParam("chefId") Integer chefId,
+                            Model model, HttpSession session) {
+        if (customerId == null || chefId == null) {
+            return "redirect:/customer_ctrl/customer_login";
+        }
+        session.setAttribute("customerId", customerId);
+        session.setAttribute("chefId", chefId);
+        model.addAttribute("customerId",customerId);
+        model.addAttribute("chefId",chefId);
+        return "chefRating";
+    }
+    @PostMapping("/submit_review")
+    public String submit_review(@RequestParam("customerId") int customerId,
+                                @RequestParam("chefId") int chefId,
+                                @RequestParam("ratingScore") int ratingScore,
+                                @RequestParam("reviewText") String reviewText,
+                                RedirectAttributes redirectAttributes )
+    {
+
+        try{
+            System.out.println("Received customerId: " + customerId);
+            System.out.println("Received chefId: " + chefId);
+            System.out.println("Received ratingScore: " + ratingScore);
+            System.out.println("Received reviewText: " + reviewText);
+
+            ChefRating  chefRating=new ChefRating();
+            Customer customer=c_rep.findById(customerId).orElseThrow(() -> new IllegalArgumentException("invalid customer id"));
+            Chef chef=ch_rep.findById(chefId).orElseThrow(()-> new IllegalArgumentException("Chef Not Found"));
+            chefRating.setChef(chef);
+            chefRating.setReview(reviewText);
+            chefRating.setCustomer(customer);
+            chefRating.setRating_score(ratingScore);
+            chefRating.setCr_date(new Date());
+            chefrating_rep.save(chefRating);
+            redirectAttributes.addFlashAttribute("message", "Review submitted successfully!");
+            return "redirect:/chef_ctrl/feedbackSuccess";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Error submitting the review. Please try again.");
+            return "redirect:/chef_ctrl/submit_review";
+        }
+    }
+    @GetMapping("/feedbackSuccess")
+    public String feedbacksuccesspage(Model model){
+        return "feedbackSuccess";
     }
 //
 
